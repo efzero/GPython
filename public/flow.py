9478f3ec11238046a2b2375d95f47f5d75f5e71d
efzero <tripofsea@gmail.com>
2017-08-03 16:54:16 -0500

    add direct graph traversal algorithm in testtest.py

--- public/flow.py
+++ public/flow.py
@@ -23,11 +23,11 @@ def read_in():
 
 def main():
 
-    # with open('test.json') as json_data:
-    #     text = json.load(json_data)
+    with open('test.json') as json_data:
+        text = json.load(json_data)
     # text['cells'][0]['ports']['groups']['out']['attrs']['.port-body']['magnet']="shabi"
     # text['cells'][0]['ports']['groups']['in']['attrs']['.port-body']['magnet']="shabi"
-    text = read_in()
+    # text = read_in()
     
 
 
@@ -47,13 +47,24 @@ def main():
     input2 = []
     dataDict = {}
     data = csvDF('mydata.csv')
+    col1 = in1[0]['data']
+    col2 = in2[0]['data']
 
+    if type(in1[0]['data']) != str:
+        col1 = ','.join(i for i in col1)
+
+    if type(in2[0]['data']) != str:
+        col2 = ','.join(i for i in col2)
+
+
+    print(col1)
+    print(col2)
 
     if in1 != []:
-        input1 =  data[in1[0]['data']]
+        input1 =  data[col1]
         dataDict['input1'] = input1
     if in2 != []:
-        input2 = data[in2[0]['data']]
+        input2 = data[col2]
         dataDict['input2'] = input2
 
 
--- public/testtest.py
+++ public/testtest.py
@@ -18,16 +18,97 @@ def getName(cells, id):
 
 # input1 = list(filter(lambda d: d['attrs']['.label']['text'] == 'input1'], cells))
 # input2 = list(filter(lambda d: d['attrs']['.label']['text'] == 'input2'], cells))
+# print(len(text['cells']))
+
+def nametoId(name):
+    return 0
+
+def Idtoname(id):
+    return 0
+
+
+def process_link(cell, cell2):
+    str = cell + cell2
+    print(str)
+
+def getBackwardCells(cell):
+    return backwardmap[cell]
+def getLinkName(link):
+    return link['labels'][0]['attrs']['text']['text']
+
+def getForwardCells(cell):
+    return forwardmap[cell]
 
 
 
-for i in range(len(links)):
-    targetName = getName(cells, links[i]['target']['id'])
-    sourceName = getName(cells, links[i]['source']['id'])
-    LinkName = links[i]['labels'][0]['attrs']['text']['text']
-    print('target', targetName)
-    print('linkname', LinkName)
-    print('source', sourceName)
+
+# for i in range(len(links)):
+#     targetName = getName(cells, links[i]['target']['id'])
+#     sourceName = getName(cells, links[i]['source']['id'])
+#     LinkName = links[i]['labels'][0]['attrs']['text']['text']
+#     print('target', targetName)
+#     print('linkname', LinkName)
+#     print('source', sourceName)
+
+
+
+
+visited = []
+stored = []
+
+def workflow(cell, direction, cells):
+    if direction == 'back':
+        #the starting point of the graph
+        if len(getBackwardCells(cell)) == 0 and cell not in visited:
+            workflow(cell, 'forward', cells)
+        else:
+        #searching for the backward nodes
+            for node in getBackwardCells(cell):
+                if node not in visited:
+                    workflow(node, 'back', cells)   
+    else:
+        #only do the traversal when the cell 
+        if cell not in visited:
+            if len(getForwardCells(cell)) == 0:
+                return
+            else:
+                visited.append(cell)
+                for node in getForwardCells(cell):
+                    process_link(cell, node)
+                    if len(getBackwardCells(node)) > 1:
+                        for i in getBackwardCells(node):
+                            workflow(i, 'back', cells)                
+                    workflow(node, 'forward', cells)
+
+                
+
+cells = ['kuangluo', 'yangtuo', 'xiaoge', 'laogou', 'bowen', 'samantha', 'ayumi', 'amanda']
+forwardmap = {'samantha':[], 'amanda':[], 'kuangluo': ['ayumi', 'amanda'], 'ayumi': ['xiaoge'], 'bowen' :['samantha'], 'yangtuo': ['ayumi', 'amanda'], 'xiaoge': ['samantha'], 'laogou': ['xiaoge']}
+backwardmap = {'samantha': ['bowen', 'xiaoge'], 'amanda': ['kuangluo', 'yangtuo'], 'ayumi': ['yangtuo', 'kuangluo'], 'kuangluo' :[], 'yangtuo': [], 'xiaoge': ['laogou','ayumi'], 'bowen': [], 'laogou': []}
+
+
+workflow('kuangluo', 'back', cells)
+
+# print(visited)
+
+
+# for i in cells:
+#     print(visited)
+#     if i not in visited:
+#         workflow(i, 'back', cells)
+
+   # print(cell, direction)
+    # print(visited)
+# print(visited)
+# print(len(getBackwardCells('samantha')))
+# for i in cells:
+#     print(getForwardCells(i))
+
+
+# print('then')
+
+# for i in cells:
+#     print(getBackwardCells(i))
 
 
 # # print(len(links))
