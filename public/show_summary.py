f343c70056499e7d2aae39e56ce5abc3fbad937a
KeyeZhang <keyez2@illinois.edu>
2017-09-13 22:05:03 -0500

    changed absolute paths to relative

--- public/show_summary.py
+++ public/show_summary.py
@@ -1,9 +1,8 @@
 import pandas as pd
-
-
 def show_summary():
     try:
-        myfile = pd.read_csv('/Users/yijiaqian/NCSAproject/GPython/mydata.csv')
+        # myfile = pd.read_csv('/Users/yijiaqian/NCSAproject/GPython/mydata.csv')
+        myfile = pd.read_csv('../mydata.csv')
         summary = list(myfile.columns.values)
         datastring = ''
         for i in summary:
